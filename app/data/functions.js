1ac5602c0ac5a6ef2dc94ee657258d94ce172a12
Willie Smalls <williesmalls@Willies-MacBook-Air.local>
2020-03-31 23:42:45 -0400

    arranged links and home.html

--- app/data/functions.js
+++ app/data/functions.js
@@ -1,13 +1,13 @@
 
-function calculateTotals(){
+function calculateTotals() {
     oBArray = document.getElementsByClassName("balance")
-    function findOBalance() {
-        oBTotal = 0;
-        for (var i = 0; i < oBArray.length; i++) {
-            if (parseInt(oBArray[i].value))
-                oBTotal += parseInt(oBArray[i].value);
-        }
-        return oBTotal;
+    oBTotal = 0;
+
+    for (var i = 0; i < oBArray.length; i++) {
+        if (parseInt(oBArray[i].value))
+            oBTotal += parseInt(oBArray[i].value);
+        console.log(oBTotal);
+
     }
-    document.getElementById("totOutBalance").innerHTML = "3,000.00"
+    document.getElementById("totOutBalance").innerHTML = 3000
 }
