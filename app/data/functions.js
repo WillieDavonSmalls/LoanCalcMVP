1b5fb3d31f8651f10cc34cfaa69a3eb1054ffa9b
Willie Smalls <williesmalls@Willies-MacBook-Air.local>
2020-04-06 00:26:11 -0400

    monthly balance formula complete with edits

--- app/data/functions.js
+++ app/data/functions.js
@@ -1,13 +1,25 @@
+//Calculate the Totals for Outstaninding balance
+function calculate_outstanding_balance() {
+    array = document.getElementsByClassName("balance")
+    total = 0;
 
-function calculateTotals() {
-    oBArray = document.getElementsByClassName("balance")
-    oBTotal = 0;
-
-    for (var i = 0; i < oBArray.length; i++) {
-        if (parseInt(oBArray[i].value))
-            oBTotal += parseInt(oBArray[i].value);
-        console.log(oBTotal);
-
+    for (var i = 0; i < array.length; i++) {
+        if (parseInt(array[i].value))
+            total += parseInt(array[i].value);
     }
-    document.getElementById("totOutBalance").innerHTML = oBTotal
+    document.getElementById("totOutBalance").innerHTML = total
 }
+
+//Calculate the total for the monthly payments
+function calculate_monthly_balance() {
+    array = document.getElementsByClassName("monthly_payment")
+    total = 0;
+
+    for (var i = 0; i < array.length; i++) {
+        if (parseInt(array[i].value))
+            total += parseInt(array[i].value);
+    }
+    document.getElementById("tot_monthly_payment").innerHTML = total
+}
+
+//TODO: Sum product for total loan and interest rate
