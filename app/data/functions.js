8877261122c69cef95fa3f9b2579c5e35f846d5a
Willie Smalls <williesmalls@Willies-MacBook-Air.local>
2020-04-07 00:05:05 -0400

    formulas working

--- app/data/functions.js
+++ app/data/functions.js
@@ -4,8 +4,9 @@ function calculate_outstanding_balance() {
     total = 0;
 
     for (var i = 0; i < array.length; i++) {
-        if (parseInt(array[i].value))
-            total += parseInt(array[i].value);
+        if (parseFloat(array[i].value)) {
+            total += parseFloat(array[i].value);
+        }
     }
     document.getElementById("totOutBalance").innerHTML = total
 }
@@ -16,10 +17,25 @@ function calculate_monthly_balance() {
     total = 0;
 
     for (var i = 0; i < array.length; i++) {
-        if (parseInt(array[i].value))
-            total += parseInt(array[i].value);
+        if (parseFloat(array[i].value)) {
+            total += parseFloat(array[i].value);
+        }
     }
     document.getElementById("tot_monthly_payment").innerHTML = total
 }
 
 //TODO: Sum product for total loan and interest rate
+function calculate_aggregate_rate() {
+    array0 = document.getElementsByClassName("balance")
+    array1 = document.getElementsByClassName("interest_rate")
+    total0 = 0;
+    total1 = 0;
+
+    for (var i = 0; i < array.length; i++) {
+        if (parseFloat(array0[i].value) && parseFloat(array1[i].value)) {
+            total0 += parseFloat(array0[i].value) * parseFloat(array1[i].value);
+            total1 += parseFloat(array0[i].value);
+        }
+    }
+    document.getElementById("tot_rate").innerHTML = (total0 / total1)
+}
